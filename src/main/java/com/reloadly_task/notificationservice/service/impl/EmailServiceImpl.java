f4b0e2ea0b6af4874ef2b3f5ad4a3f56f81ea70d
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-03-18 00:45:20 +0100

    add system.properties

--- src/main/java/com/reloadly_task/notificationservice/service/impl/EmailServiceImpl.java
+++ src/main/java/com/reloadly_task/notificationservice/service/impl/EmailServiceImpl.java
@@ -5,33 +5,47 @@ import com.reloadly_task.notificationservice.dto.NotificationResponse;
 import com.reloadly_task.notificationservice.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
+import java.util.Properties;
+
+import static org.bouncycastle.asn1.iana.IANAObjectIdentifiers.mail;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
-    JavaMailSender emailSender;
-
+    @Autowired
+    private JavaMailSender mailSender;
 
     @Override
     public NotificationResponse sendEmail(EmailNotificationRequest emailNotificationRequest) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(emailNotificationRequest.getFrom());
-            message.setTo(emailNotificationRequest.getTo());
-            message.setSubject(emailNotificationRequest.getSubject());
-            message.setText(emailNotificationRequest.getNarration());
-            this.emailSender.send(message);
-            return new NotificationResponse("Sent successfully!", true, new Date());
-        } catch (Exception e) {
-            log.info("Failed to send Notification", e.getMessage());
-        }
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
 
-        return new NotificationResponse("Sending Failed!", false, new Date());
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setFrom(emailNotificationRequest.getFrom());
+            msg.setText(emailNotificationRequest.getNarration());
+            msg.setTo(emailNotificationRequest.getTo());
+
+            msg.setSubject("Testing from Spring Boot");
+            msg.setText("Hello World \n Spring Boot Email");
+
+            mailSender.send(msg);
+            return null;
+        } catch (Exception e) {
+            log.info("Failed to send message!", e.getCause());
+        }
+        return new NotificationResponse("FAILED TO SEND NOTIFICATION", false, new Date());
     }
 }
